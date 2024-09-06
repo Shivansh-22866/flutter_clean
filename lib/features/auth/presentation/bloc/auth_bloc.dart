3741648e71521fde58794d17f3070e0db793b395
Shivansh-22866 <shivanshp0418@gmail.com>
2024-09-07 00:03:22 +0530

    Auth Bloc done

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,13 +1,20 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean/features/auth/domain/useCases/user_sign_up.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
+  final UserSignUp _userSignUp;
+  AuthBloc({
+    required UserSignUp userSignup,
+  })  : _userSignUp = userSignup,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      final res = await _userSignUp(UserSignUpParams(
+          password: event.password, email: event.email, name: event.name));
+      res.fold((fail) => emit(AuthFailure(fail.message)), (uid) => emit(AuthSuccess(uid)));
     });
   }
 }
